2cbf5b436274c6602994236a4862d89cfb602183
Jason <jasonelliott99@yahoo.com>
2019-11-12 19:21:24 -0800

    Insertion sorter / Selection Sorter

--- src/_00_Sorting_Algorithms/HeapSorter.java
+++ src/_00_Sorting_Algorithms/HeapSorter.java
@@ -15,11 +15,11 @@ public class HeapSorter extends Sorter {
 	void sort(int[] array, SortingVisualizer display) {
 		//8. create an integer called n and set it equal 
 		//   to the length of the array
-		
+		int n = array.length;
 		//9. make a for loop that starts half way between
 		//   0 and n and counts down until it is less than 0.
 		//   Inside this for loop, call the heapSort method with n and i
-        
+      //  for(int i = 0; i >)   HEL HERE
         //10. make a for loop that starts at n-1 
         //    and counts down until it is less than 0.
         
@@ -31,22 +31,34 @@ public class HeapSorter extends Sorter {
 	
 	public void heapSort(int[] array, int n, int i, SortingVisualizer display) {
 		//2. create an integer called largest and set it equal to i
-	
+	int largest = i;
 		//3. create an integer called l and set it equal to 2 * i + 1
-        
+        int l = 2 * i + 1;
 		//4. create an integer called r and set it equal to 2 * i + 2
- 
+ int r = 2 * i + 2;
         //5. if l is less than n 
         //   and array element at l is greater than array element at largest
         //   then set largest equal to l
- 
+ if(l < n && array[l] > array[largest]) {
+	 largest = l;
+ }
         //6. if r is less than n 
         //   and array element at r is greater than array element at largest
         //   then set largest equal to r
- 
+ if(r < n && array[r] > array[largest]) {
+	 largest = r;
+ }
         //7. if largest is not equal to i
         //   then swap the array elements at i and largest.
         //   Also, call the heapSort method with n and largest
+ if(largest != i) {
+	 int storage = array[i];
+	 int storelarge = largest;
+	 array[i] = storelarge;
+	 largest = storage;
+	 heapSort(array,n, largest, display);
+			 
+ }
 	}
 
 }
